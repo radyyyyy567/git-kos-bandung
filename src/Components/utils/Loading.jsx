969566102c8838cf613db3257bcf4421b33a3574
radyyyyy567 <127324238+radyyyyy567@users.noreply.github.com>
2025-01-09 11:24:52 +0700

    update: change preload animation

--- src/Components/utils/Loading.jsx
+++ src/Components/utils/Loading.jsx
@@ -1,12 +1,17 @@
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import React from 'react'
+import { loading } from '../../img'
+
 
 const Loading = () => {
   return (
     <div className="h-screen bg-white fixed top-0 left-0 flex items-center w-full font-bold text-xl justify-center space-x-2">
-        <div><FontAwesomeIcon icon={faSpinner} className='animate-spin text-indigo-400 text-2xl' /></div>
-        <div className='text-indigo-400'>Processing</div>
+        <video
+        src={loading}
+        autoPlay
+        loop
+        muted
+      />
     </div>
   )
 }
